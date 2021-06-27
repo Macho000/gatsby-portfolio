f86d52a2da276b1506d3562d200a85a4273ad453
Macho000 <saburotyan758@gmail.com>
2021-06-27 16:16:12 +0900

    add onCreateWebpackConcifg

--- gatsby-node.js
+++ gatsby-node.js
@@ -115,3 +115,17 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        path: require.resolve("path-browserify")
+      },
+      fallback: {
+        fs: false
+      }
+    }
+  });
+};
